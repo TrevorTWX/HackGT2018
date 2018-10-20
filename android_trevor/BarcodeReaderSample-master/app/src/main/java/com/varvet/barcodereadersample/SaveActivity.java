8f4c702b699fe41651fcf0792c0ff4148eb70790
TrevorMBP <trevortwx@gmail.com>
2018-10-20 18:47:30 -0400

    get path successfully

--- android_trevor/BarcodeReaderSample-master/app/src/main/java/com/varvet/barcodereadersample/SaveActivity.java
+++ android_trevor/BarcodeReaderSample-master/app/src/main/java/com/varvet/barcodereadersample/SaveActivity.java
@@ -1,8 +1,14 @@
 package com.varvet.barcodereadersample;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class SaveActivity extends Activity {
     @Override
@@ -10,6 +16,29 @@ public class SaveActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save);
         Button btn_back = findViewById(R.id.btn_dlt);
+
+//        try {
+//            openFileInput("saved_location.txt");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        try {
+            FileInputStream in = openFileInput("saved_location.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            inputStreamReader.close();
+            TextView debug = findViewById(R.id.debugsaved);
+            debug.setText(sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
 //        btn_back.setOnClickListener(new View.OnClickListener() {
 //            public void onClick(View arg0) {
