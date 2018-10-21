0a4db6955d97320ad5d8abc98e565577e83d9057
Ruohan Zhang <ruohan.zhang@outlook.com>
2018-10-20 21:21:47 -0400

    ResponseImage

--- android_trevor/BarcodeReaderSample-master/app/src/main/java/com/varvet/barcodereadersample/NavigationActivity.java
+++ android_trevor/BarcodeReaderSample-master/app/src/main/java/com/varvet/barcodereadersample/NavigationActivity.java
@@ -1,7 +1,15 @@
 package com.varvet.barcodereadersample;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 public class NavigationActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class NavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+        String url = "http://45.77.223.113/map?map=walmart&file=PNG&attach=true";
+        final ImageView map = findViewById(R.id.imageView);
+
+        RequestQueue rq = Volley.newRequestQueue(this);
+        ImageRequest ir = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        map.setImageBitmap(response);
+                    }
+                }, 0, 0, null, null,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("Error ["+error+"]");
+                        // handle the error here
+                    }
+                });
+        rq.add(ir);
     }
 }
--- android_trevor/BarcodeReaderSample-master/app/src/main/java/com/varvet/barcodereadersample/SaveActivity.java
+++ android_trevor/BarcodeReaderSample-master/app/src/main/java/com/varvet/barcodereadersample/SaveActivity.java
@@ -1,8 +1,18 @@
 package com.varvet.barcodereadersample;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.design.widget.Snackbar;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 public class SaveActivity extends Activity {
     @Override
@@ -10,6 +20,9 @@ public class SaveActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save);
         Button btn_back = findViewById(R.id.btn_dlt);
+
+
+
 
 //        btn_back.setOnClickListener(new View.OnClickListener() {
 //            public void onClick(View arg0) {
